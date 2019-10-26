b2abaf2a7c70056feeb705050ec98849eeffde0b
rakesh.up@zohocorp.com <rakesh-5128@csez.zohocorpin.com>
2019-10-26 21:15:20 +0530

    Updates

---
+++ precache-manifest.93e796fa06dd4867aea16b9ecf51045b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092eff54a34a18eb5b6d9eebf797df76",
+    "url": "/retro-snake/index.html"
+  },
+  {
+    "revision": "c4c3277c44cc1ba60596",
+    "url": "/retro-snake/static/css/main.86fe8c39.chunk.css"
+  },
+  {
+    "revision": "eb4a7daf8295c8efeb6e",
+    "url": "/retro-snake/static/js/2.2ba89e62.chunk.js"
+  },
+  {
+    "revision": "c4c3277c44cc1ba60596",
+    "url": "/retro-snake/static/js/main.b182ec34.chunk.js"
+  },
+  {
+    "revision": "c181fab1aa89aa4a090c",
+    "url": "/retro-snake/static/js/runtime-main.65393bed.js"
+  },
+  {
+    "revision": "f03febe581b686cbac1e6d5cb7677df3",
+    "url": "/retro-snake/static/media/04B_30__.f03febe5.TTF"
+  },
+  {
+    "revision": "e4968a29bb397f1dd4bdea959fa21672",
+    "url": "/retro-snake/static/media/die.e4968a29.mp3"
+  },
+  {
+    "revision": "06392d16b93014db411580ef9b57fb0c",
+    "url": "/retro-snake/static/media/food.06392d16.svg"
+  },
+  {
+    "revision": "e6cfa9be5175a34e6437a85677e86207",
+    "url": "/retro-snake/static/media/high.e6cfa9be.mp3"
+  },
+  {
+    "revision": "8ffce12452ca01bb26354974d2c48254",
+    "url": "/retro-snake/static/media/low.8ffce124.mp3"
+  }
+]);
